50715386739ec69c30ececdee1228413a8897e6e
Aman <amanadv2022@gmail.com>
2026-07-28 15:24:31 +0530

    Day 8

---
+++ client/src/pages/JobMatches.jsx
@@ -0,0 +1,9 @@
+function JobMatches() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Job Matches Page</h1>
+    </div>
+  );
+}
+
+export default JobMatches;
---
+++ client/src/pages/ResumeHistory.jsx
@@ -0,0 +1,9 @@
+function ResumeHistory() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Resume History Page</h1>
+    </div>
+  );
+}
+
+export default ResumeHistory;
